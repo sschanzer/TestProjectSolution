a628d8283e5ab8229c594f69facc1a777a588aeb
Spencer Schanzer <spencer.schanzer@gmail.com>
2025-03-22 15:47:11 -0700

    Set up of initial solution with LeetCodeProblems, ProjectEulerProblems, PersonalProblems and TestProjectTest projects. References of the three problem projects added to the TestProjectTests so we can begin unit tests.

---
+++ TestProjectSolution/TestProjectTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestProjectTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestProjectTests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("65f7dc2a-bc5d-4b21-9095-f1d37b2c3324")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
