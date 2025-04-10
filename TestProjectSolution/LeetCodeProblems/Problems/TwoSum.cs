9df1142b2468fb7e4cb65295dd3699bdff449429
Spencer Schanzer <spencer.schanzer@gmail.com>
2025-04-09 20:02:15 -0700

    Added another solution to TwoSum. Added another test.

--- TestProjectSolution/LeetCodeProblems/Problems/TwoSum.cs
+++ TestProjectSolution/LeetCodeProblems/Problems/TwoSum.cs
@@ -20,7 +20,13 @@ namespace LeetCodeProblems
     /// </remarks>
     public class TwoSum
     {
-        public int[] twoSum1(int[] nums, int target)
+        /// <summary>
+        /// Brute force solution.
+        /// </summary>
+        /// <param name="nums">The input array.</param>
+        /// <param name="target">The target int.</param>
+        /// <returns>Integer array of the indicies of the two numbeers that sum to the target.</returns>
+        public int[] TwoSum1(int[] nums, int target)
         {
             for (int i = 0; i < nums.Length; i++)
             {
@@ -35,6 +41,40 @@ namespace LeetCodeProblems
                         return solution;
                     }
                 }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Alternative solution.
+        /// </summary>
+        /// <param name="nums">The input array.</param>
+        /// <param name="target">The target int.</param>
+        /// <returns>Integer array of the indicies of the two numbeers that sum to the target.</returns>
+        public int[] TwoSum2(int[] nums, int target)
+        {
+            /// Input: nums = [2,7,11,15], target = 9
+            /// Output: [0, 1]
+            /// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+            
+            var indexMap = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var currentValue = nums[i];
+                var difference = target - currentValue;
+
+                if (indexMap.ContainsKey(difference))
+                {
+                    int idx = Array.IndexOf(nums, currentValue);
+                    var solution = new int[] { indexMap[difference], idx };
+
+                    return solution;
+                }
+
+                indexMap[currentValue] = i;
+
             }
 
             return null;
--- TestProjectSolution/TestProjectTests/LeetCodeTests/TwoSumTests.cs
+++ TestProjectSolution/TestProjectTests/LeetCodeTests/TwoSumTests.cs
@@ -10,15 +10,35 @@ namespace TestProjectTests.LeetCodeTests
 	[TestClass]
 	public class TwoSumTests
 	{
+		/// <summary>
+		/// Tests the <see cref="TwoSum.TwoSum1(int[], int)"/> method.
+		/// </summary>
 		[TestMethod]
 		[TestCategory(TestList.Validation)]
-		public void TestMethod1()
+		public void TestTwoSum_TwoSum1()
 		{
 			var twoSum = new TwoSum();
 			int[] nums = { 2, 7, 11, 15 };
 			int target = 9;
 
-			var result = twoSum.twoSum1(nums, target);
+			var result = twoSum.TwoSum1(nums, target);
+
+            Assert.AreEqual(0, result[0]);
+            Assert.AreEqual(1, result[1]);
+        }
+
+        /// <summary>
+        /// Tests the <see cref="TwoSum.TwoSum1(int[], int)"/> method.
+        /// </summary>
+        [TestMethod]
+        [TestCategory(TestList.Validation)]
+        public void TestTwoSum_TwoSum2()
+        {
+            var twoSum = new TwoSum();
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+
+            var result = twoSum.TwoSum2(nums, target);
 
             Assert.AreEqual(0, result[0]);
             Assert.AreEqual(1, result[1]);
