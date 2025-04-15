c41b14c982b3cc3255aa27c6cd8b0675c13c5232
Spencer Schanzer <spencer.schanzer@gmail.com>
2025-04-14 19:57:05 -0700

    Stupid solution to Project Euler problem 1 added. Tests added.

---
+++ TestProjectSolution/ProjectEulerProblems/MultiplesOf3And5.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerProblems
+{
+    /// <summary>
+    /// Project Euler Problem 1.
+    /// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+    /// The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+    /// </summary>
+    public class MultiplesOf3And5
+    {
+        /// <summary>
+        /// Method determines the sum of all multiples of 3 and 5 below n.
+        /// </summary>
+        /// <param name="n">The number to check up to.</param>
+        /// <returns>The sum of all multiples of 3 and 5 below n.</returns>
+        public int GetSumDumb(int n)
+        {
+            var multiplesOfThree = new List<int>();
+            for (int i = 3; i < n; i += 3)
+            {
+                multiplesOfThree.Add(i);
+            }
+
+            var multiplesOfFive = new List<int>();
+            for (int i = 5; i < n; i += 5)
+            {
+                if (!multiplesOfThree.Contains(i))
+                {
+                    multiplesOfFive.Add(i);
+                }
+            }
+
+            var multiples = multiplesOfThree.Union(multiplesOfFive);
+
+            return multiples.Sum();
+        }
+    }
+}
---
+++ TestProjectSolution/TestProjectTests/ProjectEulerTests/MultiplesOf3And5Tests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEulerProblems;
+
+namespace TestProjectTests.ProjectEulerTests
+{
+	/// <summary>
+	/// Tests for the <see cref="MultiplesOf3And5"/> class.
+	/// </summary>
+	[TestClass]
+	public class MultiplesOf3And5Tests
+	{
+		/// <summary>
+		/// Tests the <see cref="MultiplesOf3And5.GetSumDumb(int)"/> method.
+		/// </summary>
+		[TestMethod]
+        [TestCategory(TestList.Validation)]
+        public void TestMultiplesOf3And5_GetSumDumb_Test1()
+		{
+			var getMultiples = new MultiplesOf3And5();
+			var num = 10;
+			const int ExpectedResult = 23;
+
+			var result = getMultiples.GetSumDumb(num);
+			Assert.AreEqual(ExpectedResult, result);
+		}
+
+        /// <summary>
+        /// Tests the <see cref="MultiplesOf3And5.GetSumDumb(int)"/> method.
+        /// </summary>
+        [TestMethod]
+        [TestCategory(TestList.Validation)]
+        public void TestMultiplesOf3And5_GetSumDumb_Test2()
+        {
+            var getMultiples = new MultiplesOf3And5();
+            var num = 20;
+            const int ExpectedResult = 78;
+
+            var result = getMultiples.GetSumDumb(num);
+            Assert.AreEqual(ExpectedResult, result);
+        }
+
+        /// <summary>
+        /// Tests the <see cref="MultiplesOf3And5.GetSumDumb(int)"/> method.
+        /// </summary>
+        [TestMethod]
+        [TestCategory(TestList.Validation)]
+        public void TestMultiplesOf3And5_GetSumDumb_Test3()
+        {
+            var getMultiples = new MultiplesOf3And5();
+            var num = 1000;
+            const int ExpectedResult = 233168;
+
+            var result = getMultiples.GetSumDumb(num);
+            Assert.AreEqual(ExpectedResult, result);
+        }
+    }
+}
