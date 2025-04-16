6a3d852848b80e0f6e256e0d1185bcf075307218
Spencer Schanzer <spencer.schanzer@gmail.com>
2025-04-15 21:15:09 -0700

    Added Fibonacci class in PE. Added tests. All tests in validation passing.

---
+++ TestProjectSolution/ProjectEulerProblems/Problems/Fibonacci.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerProblems.Problems
+{
+    /// <summary>
+    /// Class containing methods relating to the Fibonacci sequence.
+    /// </summary>
+    public class Fibonacci
+    {
+        /// <summary>
+        /// Compiles a list of Fibonacci numbers.
+        /// </summary>
+        /// <param name="count">Number of terms of the seqeunce.</param>
+        /// <returns>A list of Fibonacci numbers.</returns>
+        public List<BigInteger> GetFibonacciNumbers(int count)
+        {
+            if (count <= 0)
+            {
+                return new List<BigInteger>();
+            }
+
+            var fibList = new List<BigInteger>() { 0 };
+
+            if (count == 1)
+            {
+                return fibList;
+            }
+
+            fibList.Add(1);
+
+            while (fibList.Count < count)
+            {
+                var nextTerm = fibList[fibList.Count - 2] + fibList[fibList.Count - 1];
+                fibList.Add(nextTerm);
+            }
+
+            return fibList;
+        }
+    }
+}
---
+++ TestProjectSolution/TestProjectTests/ProjectEulerTests/FibonacciTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEulerProblems.Problems;
+
+namespace TestProjectTests.ProjectEulerTests
+{
+	/// <summary>
+	/// Tests for the <see cref="Fibonacci"/> class.
+	/// </summary>
+	[TestClass]
+	public class FibonacciTests
+	{
+        /// <summary>
+        /// Tests the <see cref="Fibonacci.GetFibonacciNumbers(int)"/> method.
+        /// </summary>
+        [TestMethod]
+		[TestCategory(TestList.Validation)]
+		public void TestFibonacci_GetFirstTenFibonacciNumbers()
+		{
+			var fib = new Fibonacci();
+			var fibList = fib.GetFibonacciNumbers(10);
+
+			var expectedList = new List<BigInteger> { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+
+			Assert.IsTrue(expectedList.SequenceEqual(fibList));
+		}
+
+        /// <summary>
+        /// Tests the <see cref="Fibonacci.GetFibonacciNumbers(int)"/> method.
+        /// </summary>
+        [TestMethod]
+        [TestCategory(TestList.Validation)]
+        public void TestFibonacci_GetTheHundrethTermOfTheFibonacciSequence()
+        {
+            var fib = new Fibonacci();
+            var fibList = fib.GetFibonacciNumbers(100);
+
+            BigInteger expectedNumber = BigInteger.Parse("218922995834555169026");
+
+            Assert.AreEqual(expectedNumber, fibList.Last());
+        }
+
+        /// <summary>
+        /// Tests the <see cref="Fibonacci.GetFibonacciNumbers(int)"/> method.
+        /// </summary>
+        [TestMethod]
+        [TestCategory(TestList.Validation)]
+        public void TestFibonacci_GetTheThousandthTermOfTheFibonacciSequence()
+        {
+            var fib = new Fibonacci();
+            var fibList = fib.GetFibonacciNumbers(1000);
+
+            BigInteger expectedNumber = BigInteger.Parse("26863810024485359386146727202142923967616609318986952340123175997617981700247881689338369654483356564191827856161443356312976673642210350324634850410377680367334151172899169723197082763985615764450078474174626");
+
+            Assert.AreEqual(expectedNumber, fibList.Last());
+        }
+    }
+}
