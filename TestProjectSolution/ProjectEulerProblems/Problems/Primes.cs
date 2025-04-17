2a4dea547161be2a2d43b540c5220daa61328a0d
Spencer Schanzer <spencer.schanzer@gmail.com>
2025-04-16 18:56:07 -0700

    Added Primes class. Implementation of the Sieve of Eratosthenes. Added tests. All tests in validation passing.

---
+++ TestProjectSolution/ProjectEulerProblems/Problems/Primes.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerProblems.Problems
+{
+    /// <summary>
+    /// Class containing methods relating to Prime numbers.
+    /// </summary>
+    public class Primes
+    {
+        /// <summary>
+        /// Implementation of the Sieve of Eratosthenes.
+        /// </summary>
+        /// <param name="num">Represents the bound.</param>
+        /// <returns>A list of primes up to num.</returns>
+        public List<int> PrimeSieve(int num)
+        {
+            var primes = new List<int>();
+
+            // Less than zero returns an empty list.
+            if (num < 0)
+            {
+                return primes;
+            }
+
+            var sieve = new bool[num];
+
+            // Set everything int the bool array to true (so all are entries are considered prime)
+            for (int i = 0; i < sieve.Length; i++)
+            {
+                sieve[i] = true;
+            }
+
+            // Set zero and one as not prime
+            sieve[0] = sieve[1] = false;
+
+            for (int i = 2; i < Math.Pow(num, 0.5);  i++)
+            {
+                if (sieve[i])
+                {
+                    // sieve[i] is prime, so mark all multiples of sieve[i] as not prime.
+                    for (int j = i * i; j < num; j += i)
+                    {
+                        sieve[j] = false;
+                    }
+                }
+            }
+
+            // Now all indexes of the bool array with a value of true are prime
+            for (int i = 2; i < num; i++)
+            {
+                if (sieve[i])
+                {
+                    primes.Add(i);
+                }
+            }
+
+            return primes;
+        }
+    }
+}
---
+++ TestProjectSolution/TestProjectTests/ProjectEulerTests/PrimesTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEulerProblems.Problems;
+
+namespace TestProjectTests.ProjectEulerTests
+{
+	/// <summary>
+	/// Tests for the <see cref="Primes"/> class.
+	/// </summary>
+	[TestClass]
+	public class PrimesTests
+	{
+		/// <summary>
+		/// Tests the <see cref="Primes.PrimeSieve"/> method for primes up to 10.
+		/// </summary>
+		[TestMethod]
+		public void TestPrimes_PrimeSieveToTen()
+		{
+			var primes = new Primes();
+			var primesList = primes.PrimeSieve(10);
+            var expected = new List<int> { 2, 3, 5, 7 };
+
+            CollectionAssert.AreEqual(expected, primesList);
+        }
+
+        /// <summary>
+        /// Tests the <see cref="Primes.PrimeSieve"/> method for primes up to 100.
+        /// </summary>
+        [TestMethod]
+        [TestCategory(TestList.Validation)]
+        public void TestPrimes_PrimeSieveToOneHundred()
+        {
+            var primes = new Primes();
+            var primesList = primes.PrimeSieve(100);
+            var expected = new List<int>
+            {
+                2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+                31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+                73, 79, 83, 89, 97
+            };
+
+            CollectionAssert.AreEqual(expected, primesList);
+        }
+
+
+        /// <summary>
+        /// Tests the <see cref="Primes.PrimeSieve"/> method for primes up to 1000.
+        /// </summary>
+        [TestMethod]
+        [TestCategory(TestList.Validation)]
+        public void TestPrimes_PrimeSieveToOneThousand()
+        {
+            var primes = new Primes();
+            var primesList = primes.PrimeSieve(1000);
+
+            Assert.AreEqual(168, primesList.Count);
+            Assert.IsTrue(primesList.Contains(997));
+            Assert.IsFalse(primesList.Contains(1000));
+        }
+
+        /// <summary>
+        /// Tests the <see cref="Primes.PrimeSieve"/> method for primes up to 1000.
+        /// </summary>
+        [TestMethod]
+        [TestCategory(TestList.Validation)]
+        public void TestPrimes_PrimeSieveToTenThousand()
+        {
+            var primes = new Primes();
+            var primesList = primes.PrimeSieve(10000);
+
+            Assert.AreEqual(1229, primesList.Count);
+            Assert.IsTrue(primesList.Contains(9973));
+            Assert.IsFalse(primesList.Contains(10000));
+        }
+    }
+}
