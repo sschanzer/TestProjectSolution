b91158e9391565aa29349ae08eab46603bf13d7e
Spencer Schanzer <spencer.schanzer@gmail.com>
2025-05-07 18:34:31 -0700

    Clearing more warnings

--- TestProjectSolution/TestProjectTests/ProjectEulerTests/MasterTests.cs
+++ TestProjectSolution/TestProjectTests/ProjectEulerTests/MasterTests.cs
@@ -142,7 +142,6 @@ namespace TestProjectTests.ProjectEulerTests
         }
 
         /// <summary>
-        /// <para>
         /// Project Euler Problem 8.
         /// Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
         /// <see href="https://projecteuler.net/problem=8">Problem 8 description</see>.
