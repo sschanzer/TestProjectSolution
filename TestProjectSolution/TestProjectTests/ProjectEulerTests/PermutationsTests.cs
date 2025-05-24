313cb84d1009b24d3a3715aa8fdaaaf5b69041cb
Spencer Schanzer <spencer.schanzer@gmail.com>
2025-05-23 20:23:18 -0700

    Need ot update C# version...

--- TestProjectSolution/TestProjectTests/ProjectEulerTests/PermutationsTests.cs
+++ TestProjectSolution/TestProjectTests/ProjectEulerTests/PermutationsTests.cs
@@ -2,6 +2,8 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using ProjectEulerProblems.Problems;
 
@@ -21,7 +23,7 @@
         [TestCategory(TestList.Validation)]
         [DataRow("201", 3, 102)]
         [DataRow("0123456789", 1000000, 2783915460)]
-        public void TestLexicographicPermutations_GetLexicographicPermutations(string numString, int num, long answer)
+        public void TestPermutations_GetLexicographicPermutations(string numString, int num, long answer)
         {
             var permutation = Permutations.GetLexicographicPermutations(numString, num);
             var result = long.Parse(permutation[0]);
@@ -38,11 +40,45 @@
         [TestCategory(TestList.Validation)]
         [DataRow("201", 3, 102)]
         [DataRow("0123456789", 1000000, 2783915460)]
-        public void TestLexicographicPermutations_PermutationsViaHeaps(string input, int num, long expected)
+        public void TestPermutations_PermutationsViaHeaps(string input, int num, long expected)
         {
             var permutation = Permutations.PermutationsViaHeaps(input, num);
             var result = long.Parse(permutation[0]);
             Assert.AreEqual(expected, result);
+        }
+
+        /// <summary>
+        /// Tests the <see cref="Permutations.PermutationsViaHeaps(string, int)"/>.
+        /// </summary>
+        /// <param name="filePath">File path to permutation test case csv.</param>
+        [TestMethod]
+        [TestCategory(TestList.Validation)]
+        [DeploymentItem(@"ProjectEulerTests\TestData\PermutationTestCases.csv")]
+        [DataRow("PermutationTestCases.csv")]
+        public void TestPermutations_PermutationsViaHeaps_PermutationsCheck(string filePath)
+        {
+            var testCase = LoadPermutationCSV(filePath);
+
+            foreach (var test in testCase)
+            {
+                var result = Permutations.PermutationsViaHeaps(test.Input);
+                CollectionAssert.AreEqual(test.Expected, result);
+            }
+        }
+
+        /// <summary>
+        /// Tests the <see cref="Permutations.GetPermutations(string, string, List{string})"/>.
+        /// </summary>
+        /// <param name="input">Input string.</param>
+        /// <param name="expected">Expected output.</param>
+        [TestMethod]
+        [TestCategory(TestList.Validation)]
+        [DataRow("201", new int[] { 012, 021, 102, 120, 201, 210 })]
+        public void TestPermutations_GetPermutations_PermutationsCheck(string input, int[] expected)
+        {
+            var permutations = new List<string>();
+            Permutations.GetPermutations(input, string.Empty, permutations);
+            CollectionAssert.AreEqual(expected, permutations);
         }
 
         /// <summary>
@@ -55,7 +91,7 @@
         [TestCategory(TestList.Validation)]
         [DataRow("201", 3, 102)]
         [DataRow("0123456789", 1000000, 2783915460)]
-        public void TestLexicographicPermutations_GetNthPermutation(string input, int num, long expected)
+        public void TestPermutations_GetNthPermutation(string input, int num, long expected)
         {
             var permutation = Permutations.GetNthPermutation(input, num);
             var result = long.Parse(permutation[0]);
@@ -71,11 +107,32 @@
         [TestCategory(TestList.Validation)]
         [DataRow("201", 6)]
         [DataRow("abc2", 24)]
-        public void TestLexicographicPermutations_GetPermutations(string input, long expected)
+        public void TestPermutations_GetPermutations(string input, long expected)
         {
             var permutations = new List<string>();
             Permutations.GetPermutations(input, string.Empty, permutations);
             Assert.AreEqual(expected, permutations.Count);
+        }
+
+        private static List<(string Input, List<string> Expected)> LoadPermutationCSV(string filePath)
+        {
+            var lines = File.ReadAllLines(filePath).Skip(1);
+
+            var testCases = new List<(string input, List<string> expected)>();
+
+            foreach (var line in lines)
+            {
+                var parts = line.Split(',');
+
+                var input = parts[0];
+                var expectedRaw = parts.Length > 1 ? parts[1] : string.Empty;
+                var expected = expectedRaw.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToList();
+
+                testCases.Add((input, expected));
+            }
+
+            return testCases;
+
         }
     }
 }
