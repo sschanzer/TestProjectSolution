b15b22926645e4937b491cec677545b926316f97
Spencer Schanzer <spencer.schanzer@gmail.com>
2025-05-26 14:35:21 -0700

    All tests in validation and PETs passing.

---
+++ TestProjectSolution/ProjectEulerProblems/Problems/Sequence.cs
@@ -0,0 +1,37 @@
+﻿namespace ProjectEulerProblems.Problems
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Class containing methods used for calculating properties of arbitrary sequences.
+    /// </summary>
+    public static class Sequence
+    {
+        /// <summary>
+        /// Determines the distinct powers of a^b.
+        /// </summary>
+        /// <param name="baseBound">The bound on a in a^b.</param>
+        /// <param name="expBound">The bound on b in a^b.</param>
+        /// <returns>A list of distinct elements.</returns>
+        public static int FindDistinctPowers(int baseBound, int expBound)
+        {
+            HashSet<BigInteger> sequence = new HashSet<BigInteger>();
+
+            for (int i = 2; i <= baseBound; i++)
+            {
+                for (int j = 2; j <= expBound; j++)
+                {
+                    var element = BigInteger.Pow(i, j);
+                    sequence.Add(element);
+                }
+            }
+
+            return sequence.Count;
+        }
+    }
+}
---
+++ TestProjectSolution/TestProjectTests/ProjectEulerTests/SequenceTests.cs
@@ -0,0 +1,29 @@
+﻿namespace TestProjectTests.ProjectEulerTests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ProjectEulerProblems.Problems;
+
+    /// <summary>
+    /// Tests for the <see cref="Sequence"/> class.
+    /// </summary>
+    [TestClass]
+    public class SequenceTests
+    {
+        /// <summary>
+        /// Tests the <see cref="Sequence.FindDistinctPowers(int, int)"/> method.
+        /// </summary>
+        /// <param name="baseBound">Bound of the base.</param>
+        /// <param name="expBound">Bound of the exponent.</param>
+        /// <param name="expected">Expected output.</param>
+        [TestMethod]
+        [TestCategory(TestList.Validation)]
+        [DataRow(5, 5, 15)]
+        [DataRow(100, 100, 9183)]
+        public void SequenceTest_FindDistinctPowers(int baseBound, int expBound, int expected)
+        {
+            var result = Sequence.FindDistinctPowers(baseBound, expBound);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
