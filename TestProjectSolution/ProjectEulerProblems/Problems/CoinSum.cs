ee6e913ecbf3890c95b22142a56ae3880adf579e
Spencer Schanzer <spencer.schanzer@gmail.com>
2025-05-27 20:35:03 -0700

    WIP

---
+++ TestProjectSolution/ProjectEulerProblems/Problems/CoinSum.cs
@@ -0,0 +1,37 @@
+﻿namespace ProjectEulerProblems.Problems
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Class containing methods related to summing the targetValue of coins.
+    /// </summary>
+    public static class CoinSum
+    {
+        /// <summary>
+        /// Counts the number of ways to arrange coins that sum to the target targetValue.
+        /// </summary>
+        /// <param name="coins">Integer array of the coin values.</param>
+        /// <param name="targetValue">The target value.</param>
+        /// <returns>The number of ways to create the target value.</returns>
+        public static int CountCoinPermutations(int[] coins, int targetValue)
+        {
+            int length = coins.Length;
+            int[] dynamicRow = new int[targetValue + 1];
+            dynamicRow[0] = 1;
+
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = coins[i]; j <= targetValue; j++)
+                {
+                    dynamicRow[j] = dynamicRow[j - coins[i]];
+                }
+            }
+
+            return dynamicRow[targetValue];
+        }
+    }
+}
---
+++ TestProjectSolution/TestProjectTests/ProjectEulerTests/CoinSumTests.cs
@@ -0,0 +1,28 @@
+﻿namespace TestProjectTests.ProjectEulerTests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ProjectEulerProblems.Problems;
+
+    /// <summary>
+    /// Tests for the <see cref="CoinSum"/> class.
+    /// </summary>
+    [TestClass]
+    public class CoinSumTests
+    {
+        /// <summary>
+        /// Tests the <see cref="CoinSum.CountCoinPermutations(int[], int)"/> method.
+        /// </summary>
+        /// <param name="coins">Denomination of coins as an integer array.</param>
+        /// <param name="targetSum">The target sum.</param>
+        /// <param name="expected">Expected value.</param>
+        [TestMethod]
+        [TestCategory(TestList.Validation)]
+        [DataRow(new int[] { 1, 2 }, 4, 4)]
+        public void TestCoinSum_CountCoinPermutations(int[] coins, int targetSum, int expected)
+        {
+            var result = CoinSum.CountCoinPermutations(coins, targetSum);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
