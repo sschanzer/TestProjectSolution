4e0a26e12c4eb9b9810a2c5bc3f5f71bf75d0329
Spencer Schanzer <spencer.schanzer@gmail.com>
2025-07-16 19:17:08 -0700

    WIP

---
+++ TestProjectSolution/ProjectEulerProblems/Problems/DigitCancellingFractions.cs
@@ -0,0 +1,63 @@
+﻿// <copyright file="DigitCancellingFractions.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace ProjectEulerProblems.Problems
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// A class containing methods related to digit cancelling fractions.
+    /// </summary>
+    /// <see href="https://projecteuler.net/problem=33">Problem 33 description.</see>
+    public static class DigitCancellingFractions
+    {
+        /// <summary>
+        /// Finds digit cancelling fractions up to num.
+        /// </summary>
+        /// <returns>A list of digit cancelling fractions</returns>
+        public static List<(int numerator, int denominator)> FindDigitCancelingFractions(int num)
+        {
+            var results = new List<(int numerator, int denominator)>();
+
+            for (int i = 11; i < num; i++)
+            {
+                for (int j = i + 10; j <= num; j++)
+                {
+                    var numeratorString = i.ToString();
+                    var denominatorString = j.ToString();
+
+                    var oldFraction = (double)i / j;
+
+                    if (int.Parse(numeratorString[1].ToString()) != 0)
+                    {
+                        for (int k = 0; k < numeratorString.Length; k++)
+                        {
+                            var c = numeratorString[k];
+                            int indexInDenometator = denominatorString.IndexOf(c);
+
+                            if (indexInDenometator != -1)
+                            {
+                                var newNumerator = int.Parse(numeratorString.Remove(numeratorString.IndexOf(c), 1));
+                                var newDenominator = int.Parse(denominatorString.Remove(indexInDenometator, 1));
+
+                                var newFraction = (double)newNumerator / newDenominator;
+
+                                if (newFraction == oldFraction)
+                                {
+                                    results.Add((numerator: i, denominator: j));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return results;
+        }
+    }
+}
---
+++ TestProjectSolution/TestProjectTests/ProjectEulerTests/DigitCancellingFractionsTests.cs
@@ -0,0 +1,41 @@
+﻿// <copyright file="DigitCancellingFractionsTests.cs" company="MyTestProject">
+// Copyright (c) MyTestProject. All rights reserved.
+// </copyright>
+
+namespace TestProjectTests.ProjectEulerTests
+{
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ProjectEulerProblems.Problems;
+
+    /// <summary>
+    /// Test class for the <see cref="DigitCancellingFractions"/> class.
+    /// </summary>
+    [TestClass]
+    public class DigitCancellingFractionsTests
+    {
+        /// <summary>
+        /// Tests the <see cref="DigitCancellingFractions.FindDigitCancelingFractions(int)"/> method.
+        /// </summary>
+        /// <param name="bound">The number we want to find digit cancelling fractions up to.</param>
+        /// <param name="expected">The expected result.</param>
+        [TestMethod]
+        [TestCategory(TestList.Validation)]
+        [DataRow(64, new int[] { 16, 64 })]
+        [DataRow(64, new int[] { 16, 64, 19, 95, 26, 65, 49, 98 })]
+        public void TestDigitCancellingFractions_FindDigitCancelingFractions(int bound, int[] expected)
+        {
+            var result = DigitCancellingFractions.FindDigitCancelingFractions(bound);
+
+            var resultList = new List<int>();
+
+            foreach (var pair in result)
+            {
+                resultList.Add(pair.numerator);
+                resultList.Add(pair.denominator);
+            }
+
+            CollectionAssert.AreEqual(expected, resultList);
+        }
+    }
+}
